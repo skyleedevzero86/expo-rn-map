6d78630943e7a544e7c432ec27e5e28538073e02
이경용 <sleekydz86@naver.com>
2026-03-04 19:23:00 +0900

    feat: backend 프로젝트 생성

---
+++ location/src/main/kotlin/com/sleekydz86/location/LocationApplication.kt
@@ -0,0 +1,11 @@
+package com.sleekydz86.location
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LocationApplication
+
+fun main(args: Array<String>) {
+    runApplication<LocationApplication>(*args)
+}
---
+++ location/src/test/kotlin/com/sleekydz86/location/LocationApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sleekydz86.location
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LocationApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
